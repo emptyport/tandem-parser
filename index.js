2d27bc4ca79e4f4430fac573694d3d7646d0e626
Michael Porter <michaelporter.chemistry@gmail.com>
2018-06-26 09:08:25 -0500

    starting to pull values out

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var xmlConverter = require('xml-js');
 var fs = require('fs');
 
+const PROTON = 1.007276466812;
+
 module.exports.parse = function(fileText) {
   let psmList = [];
 
@@ -8,28 +10,45 @@ module.exports.parse = function(fileText) {
   var xmlObj = JSON.parse(xmlDoc);
 
   let idObjList = xmlObj.bioml.group;
+  
+  idObjList = idObjList.filter(function(idObj) {
+    return idObj['_attributes']['type'] === 'model';
+  });
+
   psmList = idObjList.map(extractInfo);
 
   return psmList;
 };
 
 extractInfo = (idObj) => {
+  let attributes = idObj['_attributes'];
+  let protein = idObj['protein'];
+  let group = idObj['group'];
+
+  var scan_title = '';
+
+  
+  group.forEach(function(g) {
+    if(g['_attributes']['type'] === 'fragment ion mass spectrum') {
+      scan_title = g['note']['_text'];
+    }
+  });
 
   let psm = {
     'sequence': '',
     'sequence_pre': '',
     'sequence_post': '',
-    'charge': '',
-    'retention_time': '',
-    'precursor_mass': '',
+    'charge': parseInt(attributes['z']),
+    'retention_time': parseFloat(attributes['rt']),
+    'precursor_mass': parseFloat(attributes['mh']) - PROTON,
     'mass_err': '',
     'theoretical_mass': '',
     'modifications': '',
     'filename': '',
-    'scan_title': '',
-    'scan_id': '',
+    'scan_title': scan_title,
+    'scan_id': attributes['id'],
     'score': '',
-    'expect': '',
+    'expect': parseFloat(attributes['expect']),
     'is_decoy': '',
     'rank': '',
     'search_engine': 'tandem'
